59af3342fdca8cb439b59a1dc346d401383dc1b4
A.Sofiane Guerfi <asofianeguerfi@calvisitor-10-105-160-122.calvisitor.1918.berkeley.edu>
2017-11-28 20:23:53 -0800

    timerset

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,13 @@
 //Trivia Game------------------------
 //variables
-var questions = [];
-var answers = [];
+var questions = ['Which is the only American state to begin with the letter "p"?', "Name the world's biggest island?",
+				"What is the world's longest river?", "Name the world's largest ocean?", 
+				"What is the diameter of Earth?", "What is the capital city of Spain?" ];
+var answers = ["Pennsylvania", "Greenland", "Amazon", "Pacific", "8,000 miles", "Madrid"];
 var correct = 0;
 var wrong = 0;
 var t = 30;
+
 
 
 $(document).ready(function(){
@@ -17,13 +20,24 @@ $(document).ready(function(){
 //start button------------------------------
 	$('#btn').click(function(){
 
-//Time remaining-----------
+		$('#btn').hide();
+		
+		//Time remaining-----------
 		function timer(){
 			t--
 			console.log(t);
+			var $time = $("<h2>Remaining Time: " +t+ " </h2> ");
+				$("#quiz").html($time);
 		};
-		setInterval(timer,1000);
+		
 		timer();
+		setInterval(timer,1000);
+		
+		
+		
+
+		
+
 		
 
 	})
